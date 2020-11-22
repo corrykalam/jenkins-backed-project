18b2267fd9911deccc89fd7a139efeb70fcf64ce
corrykalam <corry.fazidkalam@gmail.com>
2020-11-22 20:21:23 +0700

    fix

--- src/Controller/Auth.js
+++ src/Controller/Auth.js
@@ -11,13 +11,18 @@ Auth.login = async (req, res) => {
         const requestPass = req.body.password
         const check = await bcrypt.compare(requestPass, detailUsers[0].password)
         if (check) {
-            const payload = {
-                username: detailUsers[0].username,
-                role: detailUsers[0].role
+            try {
+                const payload = {
+                    username: detailUsers[0].username,
+                    role: detailUsers[0].role
+                }
+                const genToken = await model.genToken(payload)
+                await model.setToken(detailUsers[0].username, genToken)
+                return res.status(200).json({ success: true, token: genToken, message: "Logged in user successfully.", })
+            } catch (err) {
+                return res.status(500).json({ success: false, message: err })
             }
-            const genToken = await model.genToken(payload)
-            await model.setToken(detailUsers[0].username, genToken)
-            return res.status(200).json({ success: true, token: genToken, message: "Logged in user successfully.", })
+
         } else {
             return res.status(200).json({ success: true, message: "Logged in user failed." })
         }
