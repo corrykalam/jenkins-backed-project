e862e2782c16083e47952ea5fc85501fbb7a79ae
corrykalam <corry.fazidkalam@gmail.com>
2020-11-23 19:13:24 +0700

    fix

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -2,7 +2,6 @@ const express = require("express")
 const controller = require("../Controller/Auth")
 const Route = express.Router()
 
-// Route.get("/", controller.all)
 Route.post("/", controller.login)
 
 module.exports = Route
